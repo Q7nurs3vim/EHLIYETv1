9ae72b5a83fbb4f5abe818c9fcec63304a2869ee
Luz <145450869+Q7nurs3vim@users.noreply.github.com>
2023-10-03 15:20:41 +0300

    Add files via upload

---
+++ denemeV2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace denemeV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 14;
+            if (yas >= 18)
+            {
+                MessageBox.Show("EHLİYET ALABİLİRSİN :)");
+            }
+
+            if (yas <= 18)
+            {
+                MessageBox.Show("ÖNCE BÜYÜ VELET .)))))");
+            }
+
+
+
+        }
+    }
+}
